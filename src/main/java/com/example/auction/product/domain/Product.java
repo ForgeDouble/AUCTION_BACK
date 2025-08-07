81c9787afd78eef4d53966204b2cc4eec5b455c3
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-08-07 19:17:58 +0900

    feat: 상품, 태그 엔티티 생성

---
+++ src/main/java/com/example/auction/product/domain/Product.java
@@ -0,0 +1,50 @@
+package com.example.auction.product.domain;
+
+import com.example.auction.common.domain.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Product extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+    
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String productContent;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SellYN sellYN;
+    
+
+}
---
+++ src/main/java/com/example/auction/product/domain/Tag.java
@@ -0,0 +1,53 @@
+package com.example.auction.product.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Tag {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tagId;
+	
+	@OneToMany(mappedBy = "tag")
+	@Builder.Default
+    private List<Product> products = new ArrayList<>();
+	
+	@Column(nullable = false)
+	private String tagName;
+	
+	 // 부모 태그
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    @JsonBackReference
+    private Tag parent;
+
+    // 자식 태그들
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    @Builder.Default
+    private List<Tag> children = new ArrayList<>();
+}
