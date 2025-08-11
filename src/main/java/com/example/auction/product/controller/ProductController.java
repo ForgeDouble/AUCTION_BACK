4606ccb9f4427974d9bb7d588ddd76a610e16c44
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-08-11 23:35:35 +0900

    feat: product 목록 조회 컨트롤러 추가

--- src/main/java/com/example/auction/product/controller/ProductController.java
+++ src/main/java/com/example/auction/product/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.auction.product.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.auction.product.dto.ProductCreateDto;
+import com.example.auction.product.dto.ProductReadAllDto;
 import com.example.auction.product.dto.ProductReadDto;
 import com.example.auction.product.service.ProductService;
+import com.example.auction.user.dto.UserDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +28,7 @@ public class ProductController {
 	
 	private final ProductService productService;
 	    
+	// 상품 생성
     @PostMapping("/")
     public ResponseEntity<?> createProduct(@ModelAttribute ProductCreateDto productCreateDto) {
         try {
@@ -36,6 +41,7 @@ public class ProductController {
         }
     }
     
+    // 상품 단일 조회
     @GetMapping("/{productId}")
     public ResponseEntity<?> ReadProduct(@PathVariable("productId") Long productId) {
     	log.info("ReadProduct called with id={}", productId);
@@ -47,6 +53,17 @@ public class ProductController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 		} catch (Exception e) {
 			log.error("알 수 없는 서버 오류", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
+        }
+    }
+    
+    /* 상품 목록 조회 */
+    @GetMapping("/")
+    public ResponseEntity<?> ReadAllProducts() {
+        try {
+            List<ProductReadAllDto> products = productService.readAllProducts();
+            return ResponseEntity.ok(products);
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
         }
     }
