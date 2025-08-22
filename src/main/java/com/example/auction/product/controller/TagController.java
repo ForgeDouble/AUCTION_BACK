5b0a30ba5b559910c0183e56467319fa11338fc5
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-08-22 23:49:36 +0900

    Feat: tag 컨트롤러 예외 처리 방식 변경
    - CommonResDto로 공통 예외 처리

--- src/main/java/com/example/auction/product/controller/TagController.java
+++ src/main/java/com/example/auction/product/controller/TagController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.auction.common.dto.CommonResDto;
 import com.example.auction.product.dto.ProductReadDto;
 import com.example.auction.product.dto.TagCreateDto;
 import com.example.auction.product.dto.TagReadDto;
@@ -27,14 +28,8 @@ public class TagController {
 	// 태그 생성
     @PostMapping("/")
     public ResponseEntity<?> createTag(@ModelAttribute TagCreateDto tagCreateDto) {
-        try {
-			tagService.createTag(tagCreateDto);
-			return ResponseEntity.ok("태그 생성 완료");
-		} catch (RuntimeException e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-		} catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
-        }
+    	tagService.createTag(tagCreateDto);
+    	return ResponseEntity.ok(new CommonResDto(HttpStatus.OK, "태그 생성 성공", tagCreateDto));
     }
     
     //태그 삭제
@@ -46,27 +41,15 @@ public class TagController {
     // 태그 단일 조회
     @GetMapping("/{tagId}")
     public ResponseEntity<?> ReadTag(@PathVariable("tagId") Long tagId) {
-        try {
-			TagReadDto tagReadDto = tagService.getTag(tagId);
-			return ResponseEntity.ok(tagReadDto);
-		} catch (RuntimeException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		} catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
-        }
+		TagReadDto tagReadDto = tagService.getTag(tagId);
+		return ResponseEntity.ok(new CommonResDto(HttpStatus.OK, "태그 조회 성공", tagReadDto));
     }
     
     // 태그 목록 조회
     @GetMapping("/")
     public ResponseEntity<?> getAllTags() {
-    	try {
-    		List<TagReadDto> tagReadDtos = tagService.getAllTags();
-    		return ResponseEntity.ok(tagReadDtos);
-		} catch (RuntimeException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류");
-		}
+    	List<TagReadDto> tagReadDtos = tagService.getAllTags();
+    	return ResponseEntity.ok(new CommonResDto(HttpStatus.OK, "태그 목록 조회 성공", tagReadDtos));
     }
     
 }
