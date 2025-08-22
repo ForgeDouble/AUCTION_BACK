10632f9f1bb2bd72643c1ef20bdd88e7ae878c12
LEE MYEONGGYU <ikaros0569@gmail.com>
2025-08-22 15:51:21 +0900

    Feat : redis 관련 buildGradle 설정 및 config 틀 구현

---
+++ src/main/java/com/example/auction/common/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.example.auction.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+
+
+
+}
