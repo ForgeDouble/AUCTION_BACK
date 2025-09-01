1f1a8262db36a3a9ccfbe274d86710c5fbbf5d68
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-09-01 18:40:03 +0900

    Feat: wishlist 서비스 로직 보완
    - 커스텀 예외 처리 적용
    - 로그인 중인 사용자의 위시리스트 조회로 변경

--- src/main/java/com/example/auction/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/example/auction/wishlist/repository/WishlistRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.auction.wishlist.domain.Wishlist;
 
+import java.util.Optional;
+
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+    Optional<Wishlist> findByUser_UserId(Long userId);
 
 }
