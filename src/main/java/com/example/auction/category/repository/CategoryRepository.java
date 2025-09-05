473ccd74702853cc7330fad0fa81ab64c52ed384
leem5514 <ikaros0569@gmail.com>
2025-09-05 12:27:36 +0900

    Feat : 카테고리 자동 더미데이터 생성

--- src/main/java/com/example/auction/category/repository/CategoryRepository.java
+++ src/main/java/com/example/auction/category/repository/CategoryRepository.java
@@ -17,4 +17,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 //	List<Category> findAllWithChildren();
 
     List<Category> findAllByParent_CategoryId(Long parentId);
+
+    Optional<Category> findByCategoryName(String categoryName);
 }
