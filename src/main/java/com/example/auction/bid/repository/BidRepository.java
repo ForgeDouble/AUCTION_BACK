b6e57d2f0cb7466aa67f63e572af97fcb11b2e9f
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-09-06 23:46:13 +0900

    Feat: bid 에외추가
    - 이전 입찰가보다 높아야하는 예외 추가
    - 입차가를 100원 단위로 변경

--- src/main/java/com/example/auction/bid/repository/BidRepository.java
+++ src/main/java/com/example/auction/bid/repository/BidRepository.java
@@ -4,9 +4,12 @@ import com.example.auction.bid.domain.Bid;
 import com.example.auction.bid.domain.IsWinned;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
     List<Bid> findAllByProduct_ProductId(Long productId);
-    Bid findByProduct_ProductIdAndIsWinned(Long productId, IsWinned isWinned);
+    Optional<Bid> findByProduct_ProductIdAndIsWinned(Long productId, IsWinned isWinned);
+    Optional<Bid> findTopByProduct_ProductIdOrderByCreatedAtDesc(Long productId);
 }
--- src/main/java/com/example/auction/bid/service/BidService.java
+++ src/main/java/com/example/auction/bid/service/BidService.java
@@ -7,6 +7,7 @@ import com.example.auction.bid.dto.BidWinnerDto;
 import com.example.auction.bid.repository.BidRepository;
 import com.example.auction.common.domain.DelYN;
 import com.example.auction.common.exception.ResourceNotFoundException;
+import com.example.auction.common.exception.UnauthorizedAccessException;
 import com.example.auction.product.domain.Product;
 import com.example.auction.bid.dto.BidCreateDto;
 import com.example.auction.product.domain.SellYN;
@@ -58,14 +59,19 @@ public class BidService {
 
         if (product.getSellYN() != SellYN.N) {
             throw new RuntimeException("경매중인 상품이 아닙니다.");
-        }
+        } 
+//        접속중인 유저가 판매자일 경우 입찰 불가능
+//        개발중에는 주석 처리
+//        else if (product.getUser().getUserId() == user.getUserId()) {
+//            throw  new UnauthorizedAccessException("판매자는 입찰 할 수 없습니다.");
+//        } 
 
         String lockKey = "lock_bid_" + bidCreateDto.getProductId();
         RLock lock = bidRedissonClient.getLock(lockKey);
         try {
             lock.lock();
             log.info("락 획득 성공 - Key: {}", lockKey);
-            Bid bid = saveBid(bidCreateDto, user);
+            Bid bid = saveBid(bidCreateDto, user, product);
             return bid;
         } finally {
             if (lock.isHeldByCurrentThread()) {
@@ -77,9 +83,17 @@ public class BidService {
 
     // bid Insert
     @Transactional
-    protected Bid saveBid(BidCreateDto bidCreateDto, User user) {
-        Product product = productRepository.findByProductIdAndDelYn(bidCreateDto.getProductId(), DelYN.N)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 상품입니다."));
+    protected Bid saveBid(BidCreateDto bidCreateDto, User user, Product product) {
+
+        Bid recentBid = bidRepository.findTopByProduct_ProductIdOrderByCreatedAtDesc(product.getProductId())
+                .orElseThrow(() -> new ResourceNotFoundException("최신 Bid"));
+
+        if (bidCreateDto.getBidAmount() < recentBid.getBidAmount()) {
+            throw new RuntimeException("이전 입찰가 보다 높아야합니다.");
+        } else if (bidCreateDto.getBidAmount() % 100 != 0) {
+            throw  new RuntimeException("입찰가는 100원 단위여야 합니다.");
+        }
+
         Bid bid = bidCreateDto.toBid();
         bid.setProduct(product);
         bid.setUser(user);
@@ -102,8 +116,12 @@ public class BidService {
 //   예외 처리 보완 필요
     @Transactional(readOnly = true)
     public BidWinnerDto readWinner(Long productId) {
-        Bid bid = bidRepository.findByProduct_ProductIdAndIsWinned(productId, IsWinned.Y);
+        Bid bid = bidRepository.findByProduct_ProductIdAndIsWinned(productId, IsWinned.Y)
+                .orElseThrow(() -> new ResourceNotFoundException("Bid"));
+
         BidWinnerDto bidWinnerDto = BidWinnerDto.fromEntity(bid);
         return bidWinnerDto;
     }
+
+//  경매 종료 처리
 }
