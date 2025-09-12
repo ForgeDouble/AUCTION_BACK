c2bd86c52650e71d6707932efd1231509bf8c3fc
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-09-12 14:31:52 +0900

    Feat: 레디션 락 경매 -> 핫경매로 변경
    - 레디스의 zset hash를 사용한 경매
    - lua 스크립트로 원자성 보장
    - 레디스로 최근 정보를 관리
    - 레빗 mq를 사용해서 비동기적으로 디비에 저장

--- src/main/java/com/example/auction/bid/service/BidService.java
+++ src/main/java/com/example/auction/bid/service/BidService.java
@@ -3,6 +3,7 @@ package com.example.auction.bid.service;
 import com.example.auction.bid.domain.Bid;
 import com.example.auction.bid.domain.IsWinned;
 import com.example.auction.bid.dto.BidAllDto;
+import com.example.auction.bid.dto.BidEvent;
 import com.example.auction.bid.dto.BidWinnerDto;
 import com.example.auction.bid.repository.BidRepository;
 import com.example.auction.common.domain.DelYN;
@@ -14,16 +15,22 @@ import com.example.auction.product.domain.SellYN;
 import com.example.auction.product.repository.ProductRepository;
 import com.example.auction.user.domain.User;
 import com.example.auction.user.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.OptimisticLockException;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,17 +41,28 @@ public class BidService {
     private final BidRepository bidRepository;
     private final ProductRepository productRepository;
     private final UserRepository userRepository;
+    private final RedisTemplate<String, Object> bidRedisTemplate;
+    private final RedisTemplate<String, String> bidStringRedisTemplate;
+    private final BidEventProducer bidEventProducer;
+    private final ObjectMapper objectMapper;
 
     public BidService(
             @Qualifier("bidRedisson") RedissonClient bidRedissonClient,
             BidRepository bidRepository,
             ProductRepository productRepository,
-            UserRepository userRepository
-    ) {
+            UserRepository userRepository,
+            @Qualifier("bid") RedisTemplate<String, Object> bidRedisTemplate,
+            @Qualifier("bidPrice") RedisTemplate<String, String> bidStringRedisTemplate,
+            BidEventProducer bidEventProducer,
+            ObjectMapper objectMapper) {
         this.bidRedissonClient = bidRedissonClient;
         this.bidRepository = bidRepository;
         this.productRepository = productRepository;
         this.userRepository = userRepository;
+        this.bidRedisTemplate = bidRedisTemplate;
+        this.bidStringRedisTemplate = bidStringRedisTemplate;
+        this.bidEventProducer = bidEventProducer;
+        this.objectMapper = objectMapper;
     }
 
     // 입찰 서비스
@@ -64,21 +82,24 @@ public class BidService {
 //        개발중에는 주석 처리
 //        else if (product.getUser().getUserId() == user.getUserId()) {
 //            throw  new UnauthorizedAccessException("판매자는 입찰 할 수 없습니다.");
-//        } 
+//        }
 
-        String lockKey = "lock_bid_" + bidCreateDto.getProductId();
-        RLock lock = bidRedissonClient.getLock(lockKey);
-        try {
-            lock.lock();
-            log.info("락 획득 성공 - Key: {}", lockKey);
-            Bid bid = saveBid(bidCreateDto, user, product);
-            return bid;
-        } finally {
-            if (lock.isHeldByCurrentThread()) {
-                lock.unlock();
-                log.info("락 해제 성공 - Key: {}", lockKey);
-            }
-        }
+
+//      redission을 활용한 락 비즈니스 로직
+//        String lockKey = "lock_bid_" + bidCreateDto.getProductId();
+//        RLock lock = bidRedissonClient.getLock(lockKey);
+//        try {
+//            lock.lock();
+//            log.info("락 획득 성공 - Key: {}", lockKey);
+//            Bid bid = saveBid(bidCreateDto, user, product);
+//            return bid;
+//        } finally {
+//            if (lock.isHeldByCurrentThread()) {
+//                lock.unlock();
+//                log.info("락 해제 성공 - Key: {}", lockKey);
+//            }
+//        }
+        return bidHotAuction(bidCreateDto, user, product);
     }
 
     // bid Insert
@@ -105,34 +126,99 @@ public class BidService {
 
     // 핫 경매
     private Bid bidHotAuction(BidCreateDto bidDto, User user, Product product) {
-        String redisKey = "product_bid_" + product.getProductId();
+        String bidZSetKey = "product_bid_zset_" + product.getProductId();
+        String bidHashKey = "product_bid_hash_" + product.getProductId();
 
-        String luaScript = """
-        local current = tonumber(redis.call('GET', KEYS[1]) or '0')
-        local bid = tonumber(ARGV[1])
-        if bid > current then
-            redis.call('SET', KEYS[1], bid)
+        BidEvent bidEvent = BidEvent.builder()
+                .userId(user.getUserId())
+                .productId(bidDto.getProductId())
+                .bidAmount(bidDto.getBidAmount())
+                .isWinned(bidDto.getIsWinned())
+                .build();
+
+        try {
+            String bidEventJson = objectMapper.writeValueAsString(bidEvent);
+            String uuid = UUID.randomUUID().toString();
+
+            // Lua 스크립트 (최고가 비교 후 갱신)
+            String luaScript = """
+        local zsetKey = KEYS[1]
+        local hashKey = KEYS[2]
+        local uuId = ARGV[1]
+        local bidAmount = tonumber(ARGV[2])
+        local bidEventJson = ARGV[3]
+
+        -- 현재 최고가 확인
+        local currentMax = redis.call('ZREVRANGE', zsetKey, 0, 0, 'WITHSCORES')
+        local currentScore = 0
+        if #currentMax > 0 then
+            currentScore = tonumber(currentMax[2])
+        end
+
+        -- 새로운 입찰가가 더 높아야 갱신
+        if bidAmount > currentScore then
+            redis.call('ZADD', zsetKey, bidAmount, uuId)
+            redis.call('HSET', hashKey, uuId, bidEventJson)
             return 1
         else
             return 0
         end
-    """;
+        """;
 
-//        Long result = redisTemplate.execute(
-//                new DefaultRedisScript<>(luaScript, Long.class),
-//                List.of(redisKey),
-//                bidDto.getBidAmount()
-//        );
+            Long result = bidStringRedisTemplate.execute(
+                    new DefaultRedisScript<>(luaScript, Long.class),
+                    List.of(bidZSetKey, bidHashKey),
+                    uuid,
+                    String.valueOf(bidDto.getBidAmount()),
+                    bidEventJson
+            );
 
-//        if (result == 0) {
-//            throw new RuntimeException("현재가보다 높은 입찰가여야 합니다.");
-//        }
+            if (result == null || result == 0) {
+                throw new RuntimeException("현재 최고가보다 높은 금액만 입찰 가능합니다.");
+            }
 
-        // 선택: 비동기 DB 저장 (정합성 보장용)
-//        saveBidAsync(bidDto, user, product);
+            log.info("Redis ZSET + Hash 입찰 성공 - ZSET Key: {}, Hash Key: {}, UUID: {}, 입찰가: {}",
+                    bidZSetKey, bidHashKey, uuid, bidDto.getBidAmount());
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("BidEvent JSON 변환 실패", e);
+        }
+
+        // 비동기 DB 저장 (정합성 보장용)
+        bidEventProducer.publishBidEvent(bidDto, user);
 
         return bidDto.toBid(); // 임시 반환
     }
+
+
+    // 콜드 경매
+    @Transactional
+    public Bid bidColdAuction(BidCreateDto bidDto, User user, Product product) {
+        int retry = 3;
+        while (retry-- > 0) {
+            Product p = productRepository.findById(product.getProductId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Product"));
+
+            Bid recentBid = bidRepository.findTopByProduct_ProductIdOrderByCreatedAtDesc(product.getProductId())
+                    .orElse(null);
+
+            if (recentBid != null && bidDto.getBidAmount() <= recentBid.getBidAmount()) {
+                throw new RuntimeException("현재가보다 높은 입찰가여야 합니다.");
+            }
+
+            try {
+                Bid bid = bidDto.toBid();
+                bid.setUser(user);
+                bid.setProduct(product);
+                bidRepository.save(bid);
+                return bid;
+            } catch (OptimisticLockException e) {
+                // 재시도
+            }
+        }
+        throw new RuntimeException("입찰 처리 중 오류 발생, 다시 시도해주세요.");
+    }
+
 
 
 
