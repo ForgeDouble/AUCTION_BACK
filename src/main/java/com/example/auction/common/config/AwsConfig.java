2c9b6367e504305d69e850036a6607b3b9e536ab
leem5514 <ikaros0569@gmail.com>
2025-09-29 22:22:34 +0900

    Feat : AWSconfig 기본 설정 및 yml 파일 수정

---
+++ src/main/java/com/example/auction/common/config/AwsConfig.java
@@ -0,0 +1,21 @@
+package com.example.auction.common.config;
+
+import io.lettuce.core.StaticCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key:}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key:}")
+    private String secretKey;
+
+
+}
