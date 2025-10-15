d79c5c33897969dbb8398f033341e36ba57ba484
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-10-15 23:24:53 +0900

    Feat: category를 전부 받도록 dto 구조 변경

--- src/main/java/com/example/auction/product/dto/ProductReadAllDto.java
+++ src/main/java/com/example/auction/product/dto/ProductReadAllDto.java
@@ -1,5 +1,7 @@
 package com.example.auction.product.dto;
 
+import com.example.auction.category.domain.Category;
+import com.example.auction.category.dto.CategoryDto;
 import com.example.auction.product.domain.Product;
 import com.example.auction.product.domain.SellYN;
 
@@ -8,6 +10,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -21,15 +26,20 @@ public class ProductReadAllDto {
     private String productContent;
     private Long price;
     private SellYN sellYN;
+
+    private List<CategoryDto> path;
     
     public static ProductReadAllDto fromEntity(Product product) {
-    	return ProductReadAllDto.builder()
-    			.productId(product.getProductId())
-    			.categoryId(product.getCategory().getCategoryId())
-    			.productName(product.getProductName())
-    			.productContent(product.getProductContent())
-    			.price(product.getPrice())
-    			.sellYN(product.getSellYN())
-    			.build();
+        return ProductReadAllDto.builder()
+                .productId(product.getProductId())
+                .categoryId(product.getCategory().getCategoryId())
+                .productName(product.getProductName())
+                .productContent(product.getProductContent())
+                .price(product.getPrice())
+                .sellYN(product.getSellYN())
+                .path(product.getCategory().getPath().stream()
+                        .map(CategoryDto::fromEntity)
+                        .collect(Collectors.toList()))
+                .build();
     }
 }
