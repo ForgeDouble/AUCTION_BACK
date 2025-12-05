46ecd1b3fa07335c5d3f102f0361f7d56118522c
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-12-05 16:30:39 +0900

    Fix: 테스트를 위해 시간 수정
    - 준비 시간 2분 (테스트용)
    - 끝나는 시간 30분 (테스트용)
    - 상태값 바꾸는 함수 추가

--- src/main/java/com/example/auction/product/domain/Product.java
+++ src/main/java/com/example/auction/product/domain/Product.java
@@ -93,12 +93,12 @@ public class Product extends BaseTimeEntity{
     }
     // 경매 시작 시간( 상품 등록으로부터 30 분 후 자동 시작)
     public LocalDateTime getAuctionStartTime() {
-        return this.getCreatedAt().plusMinutes(30);
+        return this.getCreatedAt().plusMinutes(2);
     }
     // 경매 종료 시간 계산 (24시간 고정)
     // 상품 등록으로부터 24시간 30분 후
     public LocalDateTime getAuctionEndTime() {
-        return this.getCreatedAt().plusMinutes(30).plusHours(24);
+        return this.getCreatedAt().plusMinutes(30);
     }
 
     // 경매 상태 체크
@@ -124,5 +124,12 @@ public class Product extends BaseTimeEntity{
             return Duration.ZERO;
         }
         return Duration.between(now, endTime);
+    }
+
+    /**
+     * 상태 변경 메서드
+     */
+    public void updateStatus(Status newStatus) {
+        this.status = newStatus;
     }
 }
