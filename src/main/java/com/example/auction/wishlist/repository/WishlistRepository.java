8dfa0ad96dc7387e7fcec7ba22d796d6fac89f95
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-12-22 13:08:45 +0900

    Feat: wishlist select문 추가
    - productId 와 userId 통해 wishlistId 조회

--- src/main/java/com/example/auction/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/example/auction/wishlist/repository/WishlistRepository.java
@@ -3,6 +3,8 @@ package com.example.auction.wishlist.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.auction.wishlist.domain.Wishlist;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,4 +14,11 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     // 사용자 중복 제어
     boolean existsByUser_UserIdAndProduct_ProductId(Long userId, Long productId);
     Optional<Wishlist> findByWishlistId(Long wishlistId);
+    // userId 와 productId를 통해 whishlistId를 조회
+    @Query("SELECT w.wishlistId FROM Wishlist w WHERE w.user.userId = :userId AND w.product.productId = :productId")
+    Optional<Long> findWishlistIdByUser_UserIdAndProduct_ProductId(
+            @Param("userId") Long userId,
+            @Param("productId") Long productId
+    );
+
 }
