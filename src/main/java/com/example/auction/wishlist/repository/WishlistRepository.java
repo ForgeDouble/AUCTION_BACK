681bfc18063fcbc2a6c787a1631823df88cb6074
kkameoo <116774845+kkameoo@users.noreply.github.com>
2025-12-29 19:37:57 +0900

    Fix: 주석 수정
    - 더 깔끔하게 변경

--- src/main/java/com/example/auction/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/example/auction/wishlist/repository/WishlistRepository.java
@@ -14,11 +14,11 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     // 사용자 중복 제어
     boolean existsByUser_UserIdAndProduct_ProductId(Long userId, Long productId);
     Optional<Wishlist> findByWishlistId(Long wishlistId);
-    // userId 와 productId를 통해 whishlistId를 조회
+
+    /* userId 와 productId를 통해 whishlistId를 조회 */
     @Query("SELECT w.wishlistId FROM Wishlist w WHERE w.user.userId = :userId AND w.product.productId = :productId")
     Optional<Long> findWishlistIdByUser_UserIdAndProduct_ProductId(
             @Param("userId") Long userId,
             @Param("productId") Long productId
     );
-
 }
