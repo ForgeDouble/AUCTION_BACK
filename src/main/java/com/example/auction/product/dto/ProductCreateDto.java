a752cbf23b3f256a63c73e83fa8101aa51afd748
kkameoo <116774845+kkameoo@users.noreply.github.com>
2026-01-29 22:48:37 +0900

    Feat: ProductCreateDto 검증 추가
    - 길이 제한
    - null 검증
    - 가격 검증
    - 위 어노테이션 적용

--- src/main/java/com/example/auction/product/dto/ProductCreateDto.java
+++ src/main/java/com/example/auction/product/dto/ProductCreateDto.java
@@ -1,13 +1,19 @@
 package com.example.auction.product.dto;
 
+import com.example.auction.common.validation.annotation.ThousandUnit;
 import com.example.auction.product.domain.Product;
 import com.example.auction.product.domain.Status;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -15,10 +21,20 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class ProductCreateDto {
+
+    @NotNull
 	private Long categoryId;
+    @NotBlank
+    @Size(min = 1, max = 30)
 	private String productName;
+    @NotBlank
+    @Size(min = 1, max = 2500)
     private String productContent;
+    @NotNull
+    @Min(1000)
+    @ThousandUnit
     private Long price;
+    @NotBlank
     private Status status;
     
     public Product toProduct() {
