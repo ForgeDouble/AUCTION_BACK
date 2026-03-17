b1d3c537f70bd7095fac409f77af29e1aab3cc37
leem5514 <ikaros0569@gmail.com>
2026-03-17 21:24:58 +0900

    Feat : config 구성

---
+++ src/main/java/com/example/auction/common/config/OpenSearchConfig.java
@@ -0,0 +1,66 @@
+package com.example.auction.common.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PreDestroy;
+import org.apache.hc.client5.http.auth.AuthScope;
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
+import org.apache.hc.core5.http.HttpHost;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenSearchConfig {
+
+    @Value("${opensearch.host}")
+    private String host;
+
+    @Value("${opensearch.port}")
+    private int port;
+
+    @Value("${opensearch.scheme:http}")
+    private String scheme;
+
+    @Value("${opensearch.username}")
+    private String username;
+
+    @Value("${opensearch.password}")
+    private String password;
+
+    private OpenSearchTransport transport;
+
+    @Bean
+    public OpenSearchClient openSearchClient(ObjectMapper objectMapper) {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                new AuthScope(null, -1),
+                new UsernamePasswordCredentials(username, password.toCharArray())
+        );
+
+        HttpHost httpHost = new HttpHost(scheme, host, port);
+
+        ApacheHttpClient5TransportBuilder builder =
+                ApacheHttpClient5TransportBuilder.builder(httpHost);
+
+        builder.setMapper(new JacksonJsonpMapper(objectMapper));
+        builder.setHttpClientConfigCallback(httpClientBuilder ->
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+        );
+
+        this.transport = builder.build();
+        return new OpenSearchClient(transport);
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        if (transport != null) {
+            transport.close();
+        }
+    }
+}
